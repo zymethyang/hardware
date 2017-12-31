c875010f04b0bf19a1500293c338342ca564388a
Nguyen Minh Tri <zymethyang@gmail.com>
2017-12-31 19:17:21 +0700

    on heroku

--- src/bumpRouter.js
+++ src/bumpRouter.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const bumpRouter = express.Router();
 bumpRouter.use(bodyParser.json());
 
-const mongoose = require('mongoose');
 const Bumps = require('./models/bumps');
 const firebase = require('firebase');
 var FieldValue = require('firebase-admin').firestore.FieldValue;
@@ -27,7 +26,7 @@ bumpRouter.route('/')
                 res.statusCode = 200;
             })
             .catch(err =>{
-                console.log(user.uid + ' Fail to GET Bump Status !' + err);
+                console.log(user.uid + ' Fail to GET Bump Status ! ' + err);
                 res.statusCode = 403;
                 res.json('Error');
             });
@@ -59,7 +58,7 @@ bumpRouter.route('/')
             next();
         })
         .get((req, res) => {
-            var user = firebase.auth().currentUser;
+            var user = firebase.auth().currentUser || false;
             if (user) {
                 console.log(user.uid + ' GET Bump Status ! at ' + moment(FieldValue.serverTimestamp()).format("YYYY-MM-DD hh:mm a"));
                 Bumps.find({ $and: [{ uid: user.uid }, { 'status.from': { $lte: moment(FieldValue.serverTimestamp()).unix() } } , { 'status.to': { $gte: moment(FieldValue.serverTimestamp()).unix() } } , { 'status.cCalender': { $eq: true } }] }).sort({ updatedAt: -1 })
@@ -69,7 +68,7 @@ bumpRouter.route('/')
                   res.json(bump.reverse());
                 })
                 .catch(err => {
-                    console.log(user.uid + ' Fail to GET Bump Status !');
+                    console.log(user.uid + ' Fail to GET Bump Status ! ' +err);
                     res.statusCode = 403;
                     res.setHeader('Content-Type', 'application/json');
                     res.json('Error');
@@ -102,7 +101,7 @@ bumpRouter.route('/')
             next();
         })
         .get((req, res) => {
-            var user = firebase.auth().currentUser;
+            var user = firebase.auth().currentUser || false;
             if (user) {
                 console.log(user.uid + ' GET Bump Status ! at ' + moment(FieldValue.serverTimestamp()).format("YYYY-MM-DD hh:mm a"));
                 Bumps.findOne({ $and: [{ uid: user.uid },{ 'status.cTemp': { $eq: true } }] }).sort({ updatedAt: -1 })
@@ -112,7 +111,7 @@ bumpRouter.route('/')
                   res.json(bump.reverse());
                 })
                 .catch(err => {
-                    console.log(user.uid + ' Fail to GET Bump Status !');
+                    console.log(user.uid + ' Fail to GET Bump Status ! '+err);
                     res.statusCode = 403;
                     res.setHeader('Content-Type', 'application/json');
                     res.json('Error');
@@ -145,7 +144,7 @@ bumpRouter.route('/')
             next();
         })
         .get((req, res) => {
-            var user = firebase.auth().currentUser;
+            var user = firebase.auth().currentUser || false;
             if (user) {
                 console.log(user.uid + ' GET Bump Status ! at ' + moment(FieldValue.serverTimestamp()).format("YYYY-MM-DD hh:mm a"));
                 Bumps.findOne({ $and: [{ uid: user.uid },{ 'status.cHumidity': { $eq: true } }] }).sort({ updatedAt: -1 })
@@ -155,7 +154,7 @@ bumpRouter.route('/')
                   res.json(bump.reverse());
                 })
                 .catch(err => {
-                    console.log(user.uid + ' Fail to GET Bump Status !');
+                    console.log(user.uid + ' Fail to GET Bump Status ! '+err);
                     res.statusCode = 403;
                     res.setHeader('Content-Type', 'application/json');
                     res.json('Error');
