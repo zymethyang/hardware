715e9fdd5a19018f7a3d4e0bd05a20b4fd0aa99c
Nguyen Minh Tri <zymethyang@gmail.com>
2018-01-19 08:44:49 +0700

    add new feature

--- src/tempRouter.js
+++ src/tempRouter.js
@@ -4,7 +4,6 @@ const tempRouter = express.Router();
 tempRouter.use(bodyParser.json());
 
 const Temps = require('./models/temps');
-const firebase = require("firebase");
 var FieldValue = require("firebase-admin").firestore.FieldValue;
 var moment = require('moment');
 
