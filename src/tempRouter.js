ef40c8b8fe4b42af60ec5a0edaefd8e92635dc13
Nguyen Minh Tri <zymethyang@gmail.com>
2018-02-01 20:44:42 +0700

    edit push temporatory

--- src/tempRouter.js
+++ src/tempRouter.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const tempRouter = express.Router();
 tempRouter.use(bodyParser.json());
 
+const Humiditys = require('./models/humiditys');
 const Temps = require('./models/temps');
 var FieldValue = require("firebase-admin").firestore.FieldValue;
 var moment = require('moment');
@@ -23,12 +24,42 @@ tempRouter.route('/:uid')
     .post((req, res, next) => {
         var uid = req.params.uid || false;
         if (uid) {
+
+            var keyList = Object.keys(req.body);
+            var newData = new Array(0);
+            var newTemp = new Object;
+            var newHumidity = new Object;
+
+            for (var i = 0; i < keyList.length; i++) {
+                newData.push(req.body[keyList[i]]);
+            }
+            for (var i = 0; i < keyList.length; i++) {
+                if(newData[i][0]!==0){
+                    newTemp[keyList[i]] = newData[i][0];
+                }
+            }
+            for (var i = 0; i < keyList.length; i++) {
+                if(newData[i][1]!==0){
+                    newHumidity[keyList[i]] = newData[i][1];
+                }
+            }
+
             console.log(uid + ' POST Temperature at ' + moment(FieldValue.serverTimestamp()).format("YYYY-MM-DD hh:mm a"));
             const reducer = (accumulator, currentValue) => accumulator + currentValue;
-            var mean = Math.round(Object.values(req.body).reduce(reducer) / (Object.values(req.body).length));
+            var mean = Math.round(Object.values(newTemp).reduce(reducer) / (Object.values(newTemp).length));
             Temps.create({
                 uid: uid,
-                temp: req.body,
+                temp: newTemp,
+                mean: mean,
+                startedAt: moment(FieldValue.serverTimestamp()).unix(),
+                updatedAt: moment(FieldValue.serverTimestamp()).unix()
+            })
+
+            console.log(uid + ' POST Humidity at ' + moment(FieldValue.serverTimestamp()).format("YYYY-MM-DD hh:mm a"));
+            var mean =  Math.round(Object.values(newHumidity).reduce(reducer) / (Object.values(newHumidity).length));
+            Humiditys.create({
+                uid: uid,
+                humidity: newHumidity,
                 mean: mean,
                 startedAt: moment(FieldValue.serverTimestamp()).unix(),
                 updatedAt: moment(FieldValue.serverTimestamp()).unix()
@@ -37,11 +68,12 @@ tempRouter.route('/:uid')
                 res.setHeader('Content-Type', 'application/json');
                 res.json('Successful');
             }).catch(function (error) {
-                res.statusCode = 403;
+                res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json');
                 res.json('Error');
-                console.error(uid + ' POST TEMP error at ' + moment(FieldValue.serverTimestamp()).format("YYYY-MM-DD hh:mm a"), error);
+                console.error(user.uid + ' POST error at ' + moment(FieldValue.serverTimestamp()).format("YYYY-MM-DD hh:mm a"), error);
             });
+
         } else {
             console.log('Fail to POST TEMPORATURE AT ' + moment(FieldValue.serverTimestamp()).format("YYYY-MM-DD hh:mm a"));
             res.statusCode = 403;
